e702869bbc6a3d00acfbe88c6c0e34d7ff528960
Divyanshgupta030 <145568562+Divyanshgupta030@users.noreply.github.com>
2025-03-18 21:47:37 +0530

    init ws

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import http from "http";
 import WebSocket, { WebSocketServer } from "ws";
+import { UserManager } from "./userManager";
+import { RoomManager } from "./roomManager";
 
 const app = express();
 const server = http.createServer(app);
@@ -10,17 +12,26 @@ app.get("/", (req, res) => {
   res.send("WebSocket server is running");
 });
 
+const users = new UserManager();
+const rooms = new RoomManager();
 wss.on("connection", (ws) => {
   console.log("Client connected");
 
   ws.on("message", (message) => {
     console.log(`Received: ${message}`);
-
-    wss.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(message);
-      }
-    });
+    const data = JSON.parse(message.toString());
+    if(!users.getUser(data.userId)) {
+     users.addUser(data.user, data.user.ws)
+    }
+    switch (data.type) {
+      case "join":
+        const room = rooms.addRoom(data.room, data.roomCode);
+      
+        users.addUserToRoom(data.userId, room);
+        rooms.addMember(data.roomId, data.roomCode);
+        ws.send(JSON.stringify({ type: "join",room: room }));
+        break;
+    }
   });
 
   ws.on("close", () => {
